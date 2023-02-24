227687d135147a30ed008d49a2f2fe8037ac91c0
Vanja Kavgić <s51637@edu.campus02.at>
2023-02-24 11:00:56 +0100

    changes on 24.02

---
+++ MyProjectRecipeBook/Ingridients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectRecipeBook
+{
+    class Ingridients
+    {
+        public int IngridientId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
--- MyProjectRecipeBook/MainWindow.xaml.cs
+++ MyProjectRecipeBook/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace MyProjectRecipeBook
         public MainWindow()
         {
             InitializeComponent();
+            var ctx = new MyDataContext();
+                
+            this.DataContext = ctx;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
